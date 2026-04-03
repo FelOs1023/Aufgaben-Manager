77c54aac5c81d9ca02071ee5103b8229cbd48f6d
Oskar Salzburg <oskar@salzburg.link>
2026-04-03 16:42:27 +0200

    Fokus entfernt und load erstellt

--- src/Saving.py
+++ src/Saving.py
@@ -27,47 +27,10 @@ def AddSave(FullSaveData):
     with open(SaveData, "w", encoding="utf-8") as file:
         json.dump(data, file, indent=4)
 
-def FokusSave(FullFokusData):
-    print("Saving Fokus...\n")
-
-    if os.path.exists(SaveData):
-        with open(SaveData, "r", encoding="utf-8") as file:
-            data = json.load(file)
-    else:
-        data = []
-
-    if not data:
-        data.append({"Fokus": {}})
-    elif "Fokus" not in data[0]:
-        data[0]["Fokus"] = {}
-
-    if len(data[0]["Fokus"]) >= 3:
-        overrideFokusSave(FullFokusData)
-        return
-    
-    for key, value in FullFokusData.items():
-        if len(data[0]["Fokus"]) < 3:
-            data[0]["Fokus"][key] = value
-        else:
-            break
-
-    print("Fokus saved\n")
-
-    with open(SaveData, "w", encoding="utf-8") as file:
-        json.dump(data, file, indent=4)
-
-def overrideFokusSave(FullFokusData):
-    print("Overriding Fokus...\n")
-
-    if os.path.exists(SaveData):
-        with open(SaveData, "r", encoding="utf-8") as file:
-            data = json.load(file)
-    else:
-        data = [{"Fokus": {}}]
-
-    data[0]["Fokus"] = FullFokusData
-
-    print("Fokus overridden\n")
-
-    with open(SaveData, "w", encoding="utf-8") as file:
-        json.dump(data, file, indent=4)
+def loadSave():
+    try:
+        with open(SaveData, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except Exception as e:
+        print(f"Fehler beim Laden: {e}")
+        return []
