bb17e147bd2c2c617427f956b27eb062280c4451
Oskar Salzburg <oskar@salzburg.link>
2026-05-14 11:27:18 +0200

    Funktion entfernt und aufgeräumt

--- src/Saving.py
+++ src/Saving.py
@@ -28,19 +28,3 @@ def AddSave(FullSaveData):
         json.dump(data, file, indent=4)
 
 
-def loadInfo(self):
-    with open(SaveData, "r", encoding="utf-8") as file:
-        data = json.load(file)
-
-    search_title = "Testing New Feature"
-
-    for item in data:
-        for task_name, task in item.get("Tasks", {}).items():
-            if task.get("Task Title") == search_title:
-                self.title = task.get("Task Title")
-                self.description = task.get("Task Description")
-                self.end_date = task.get("Task End Date")
-                self.fokus_date = task.get("Task Fokus Date")
-                self.priority = task.get("Task Priority")
-                self.status = task.get("Task Status")
-
